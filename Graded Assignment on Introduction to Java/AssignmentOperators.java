4a59a671018dae7aca80bd9b56641d9ada6fa01e
sanjay kumar mundari <50072864+flamekaiser9097@users.noreply.github.com>
2023-11-18 16:42:19 +0530

    Add files via upload

---
+++ Graded Assignment on Introduction to Java/AssignmentOperators.java
@@ -0,0 +1,15 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int num1=10;
+        num1 +=2;
+        System.out.println("After addition: num = " + num1);
+        num1 -=4;
+        System.out.println("After subtraction: num = " + num1);
+        num1 *=8;
+        System.out.println("After multiplication: num = " + num1);
+        num1 /=4;
+        System.out.println("After division: num = " + num1);
+        num1 %=7;
+        System.out.println("After  modulus: num = " + num1);
+    }
+}
---
+++ Graded Assignment on Introduction to Java/RelationalOperators.java
@@ -0,0 +1,10 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int num1=10; 
+        int num2=20;
+        System.out.println("Equals: "+ (num1 == num2));
+        System.out.println("Not equals: "+ (num1 != num2));
+        System.out.println("num1 is Greater Than num2: "+ (num1 <= num2));
+        System.out.println("num1 is Less Than num2: "+ (num1 >= num2));
+    } 
+}
